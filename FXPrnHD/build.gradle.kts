f5e28cb4c8a5ed4a53208c283d1deb4c39035657
Kayo-PA <ayushku.ak1235@gmail.com>
2026-09-13 15:06:14 +0530

    stash update

--- FXPrnHD/build.gradle.kts
+++ FXPrnHD/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 3
+version = 1
 dependencies {
     implementation("androidx.annotation:annotation-jvm:1.10.0")
 }
--- Stash/build.gradle.kts
+++ Stash/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 8
+version = 1
 dependencies {
     implementation("androidx.annotation:annotation-jvm:1.9.1")
 
